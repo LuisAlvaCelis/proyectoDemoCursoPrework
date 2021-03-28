bbbb82df94629fe95ac07ebff3fbc924202c6914
luisalvacelis <luisalvacelis@gmail.com>
2021-03-28 09:40:38 -0500

    Cambios: Agregamos un boton

---
+++ script.js
@@ -0,0 +1,16 @@
+function secondLargetNumber(numbers){
+    let first = numbers[0]
+    let second = 0;
+    for(let i=0; i < numbers.length; i++) {
+      second=first;
+      first=numbers[i];
+    }
+    if(numbers[i]>second && numbers[i]<first){
+      second=numbers[i];
+    }
+    return second;
+  }
+  let nums=[8,4,10,9,11];
+  console.log(secondLargetNumber(nums))
+
+  
